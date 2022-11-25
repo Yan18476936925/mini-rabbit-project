2e4f04225148a1ec7a3f7612c02781dd0b578575
Jordan <2896990897@qq.com>
2022-11-25 11:47:07 +0800

    uni-app存储数据使用

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -20,6 +20,20 @@ export default {
     handleTap() {
       console.log("----->21", Date.now());
       this.$store.commit('setText',Date.now())
+      // 微信小程序中 本地存储
+      // 支持任意的数据类型  不需要想网页本地存储一样都需要转成字符串
+      // 存数据
+      uni.setStorageSync("abcd", 1234);
+      uni.setStorageSync("abcd1", "aaaa");
+      uni.setStorageSync("abcd2", true);
+      uni.setStorageSync("abcd3", [123, 4, 4]);
+      uni.setStorageSync("abcd4", { a: 1, b: 2 });
+      // 取数据-不可以持久化-需要安装插件
+      console.log(uni.getStorageSync("abcd"));
+      console.log(uni.getStorageSync("abcd1"));
+      console.log(uni.getStorageSync("abcd2"));
+      console.log(uni.getStorageSync("abcd3"));
+      console.log(uni.getStorageSync("abcd4"));
     },
   },
 };
