601335982abcec84e9ec819055f32444b8d353dc
Jordan <2896990897@qq.com>
2022-11-25 10:52:15 +0800

    练习vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,9 @@ import Vuex from "vuex";
 Vue.use(Vuex); //vue的插件机制
 //Vuex.Store 构造器选项
 const store = new Vuex.Store({
-  state: {},
+  state: {
+    text:'Hello Jordan'
+  },
   getters: {},
   modules: {},
 });
